c6de0392ebb9dd7cd5d057d30518f24036fea069
Alireza Josheghani <josheghani.dev@gmail.com>
2021-04-04 03:02:53 +0430

    Adding packet bytes to Packet class

--- protocol/packet.js
+++ protocol/packet.js
@@ -5,15 +5,16 @@ const EMsgMask  = 2147483647;
 
 class Packet {
   constructor(emsg, body) {
-    this.body = body;
-    let data = new_protobuf_msg(emsg, body);
-    this.data = Reader.create(Buffer.from(data));
-    let rawEMsg = this.data.fixed32() >>> 0;
-    this.emsg = rawEMsg & EMsgMask;
+    this.body  = body;
+    this.emsg = emsg
+    this.bytes = new_protobuf_msg(emsg, body);
     return this;
   }
   payload() {
-    return this.body.constructor.decode(this.data)
+    const data = Reader.create(Buffer.from(this.bytes));
+    let rawEMsg = data.fixed32() >>> 0;
+    this.emsg = rawEMsg & EMsgMask;
+    return this.body.constructor.decode(data)
   }
   is_proto() {
     return this.emsg !== proto.EMSG.INVALID
